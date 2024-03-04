da24cfe11f155b412280e80accca0c0c6e7bc7fa
GeoffreyXue <xuegeoff@gmail.com>
2024-03-04 13:57:10 -0600

    Decrease max zoom

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -25,7 +25,7 @@ const Map: React.FC<MapProps> = ({ apiKey, stakeholders }) => {
   const markersLayer = useRef(null)
 
   return (
-    <MapContainer className="h-full w-full" center={[20, 0]} zoom={3} minZoom={3} scrollWheelZoom={true} zoomControl={false} maxBounds={maxBounds}>
+    <MapContainer className="h-full w-full" center={[20, 0]} zoom={2} minZoom={2} scrollWheelZoom={true} zoomControl={false} maxBounds={maxBounds}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url={`https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png?api_key=${apiKey}`}
