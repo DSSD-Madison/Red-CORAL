4fae5c31c0a52c8db6e1229743f8104f7de59659
Tsuni <akamitsunami@gmail.com>
2024-04-22 16:44:08 -0500

    List + hide countries and departments

--- src/components/controls/CountryControl.tsx
+++ src/components/controls/CountryControl.tsx
@@ -18,20 +18,52 @@ const CountryControl: React.FC<FilterControlProps> = ({ data, filters, setFilter
   }
 
   useEffect(() => {
-    document.addEventListener('mousedown', handleClickOutside)
+    document.addEventListener('pointerdown', handleClickOutside)
     return () => {
-      document.removeEventListener('mousedown', handleClickOutside)
+      document.removeEventListener('pointerdown', handleClickOutside)
     }
   }, [])
 
-  const handleCheckboxChange = (name: string) => {
-    setFilters((filters) => ({
-      ...filters,
-      hideCountries: [...filters.hideCountries, name],
-    }))
+  const visibleDepartments = Object.entries(data.filterBounds.locations)
+    .filter(([country]) => !filters.hideCountries.includes(country))
+    .reduce(
+      (acc, [country, departments]) => {
+        acc[country] = Object.keys(departments).sort()
+        return acc
+      },
+      {} as { [country: string]: string[] }
+    )
+
+  const handleConCheckboxChange = (key: string, checked: boolean) => {
+    if (checked) {
+      setFilters((filters) => ({
+        ...filters,
+        hideCountries: filters.hideCountries.filter((el) => el !== key),
+      }))
+    } else {
+      setFilters((filters) => ({
+        ...filters,
+        hideCountries: [...filters.hideCountries, key],
+        hideDepartments: filters.hideDepartments.filter((dep) => !visibleDepartments[key].includes(dep)),
+      }))
+    }
   }
 
-  const handleSelectAll = (checked: boolean) => {
+  const handleDepCheckboxChange = (key: string, checked: boolean) => {
+    if (checked) {
+      setFilters((filters) => ({
+        ...filters,
+        hideDepartments: filters.hideDepartments.filter((el) => el !== key),
+      }))
+    } else {
+      setFilters((filters) => ({
+        ...filters,
+        hideDepartments: [...filters.hideDepartments, key],
+      }))
+    }
+  }
+
+  const handleConSelectAll = (checked: boolean) => {
     if (checked) {
       setFilters((filters) => ({
         ...filters,
@@ -45,15 +77,19 @@ const CountryControl: React.FC<FilterControlProps> = ({ data, filters, setFilter
     }
   }
 
-  const visibleDepartments = Object.entries(data.filterBounds.locations)
-    .filter(([country]) => !filters.hideCountries.includes(country))
-    .reduce(
-      (acc, [country, departments]) => {
-        acc[country] = Object.keys(departments).filter((department) => !filters.hideDepartments.includes(department))
-        return acc
-      },
-      {} as { [country: string]: string[] }
-    )
+  const handleDepSelectAll = (checked: boolean) => {
+    if (checked) {
+      setFilters((filters) => ({
+        ...filters,
+        hideDepartments: [],
+      }))
+    } else {
+      setFilters((filters) => ({
+        ...filters,
+        hideDepartments: Object.values(visibleDepartments).flat(),
+      }))
+    }
+  }
 
   return (
     <div className="leaflet-bar relative rounded">
@@ -69,8 +105,9 @@ const CountryControl: React.FC<FilterControlProps> = ({ data, filters, setFilter
         C
       </a>
       {isDropdownVisible && (
-        <div ref={dropdownRef}>
-          <div className="leaflet-bar absolute -top-0.5 left-10 box-content h-96 w-60 rounded bg-tint-02/80 shadow-lg backdrop-blur-sm">
+        <div ref={dropdownRef} className="absolute -top-0.5 left-10 flex h-96 gap-5">
+          {/* Countries */}
+          <div className="leaflet-bar box-content w-max rounded bg-tint-02/80 shadow-lg backdrop-blur-sm">
             <div className="flex flex-row justify-between">
               <div className="m-1 text-base font-semibold">Países</div>
               <div className="mx-1 flex flex-row justify-center align-middle">
@@ -81,7 +118,7 @@ const CountryControl: React.FC<FilterControlProps> = ({ data, filters, setFilter
                   type="checkbox"
                   name="all"
                   checked={Object.keys(filters.hideCountries).length === 0}
-                  onChange={(e) => handleSelectAll(e.target.checked)}
+                  onChange={(e) => handleConSelectAll(e.target.checked)}
                 />
               </div>
             </div>
@@ -93,7 +130,7 @@ const CountryControl: React.FC<FilterControlProps> = ({ data, filters, setFilter
                     name={country}
                     id={country}
                     checked={!filters.hideCountries?.includes(country)}
-                    onChange={() => handleCheckboxChange(country)}
+                    onChange={(e) => handleConCheckboxChange(country, e.target.checked)}
                     className="mr-2"
                   />
                   <label htmlFor={country} className="w-full">
@@ -103,21 +140,50 @@ const CountryControl: React.FC<FilterControlProps> = ({ data, filters, setFilter
               ))}
             </div>
           </div>
+          {/* Departments */}
+          <div className="leaflet-bar box-content w-max rounded bg-tint-02/80 shadow-lg backdrop-blur-sm">
+            <div className="flex h-8 flex-row justify-between">
+              <div className="m-1 text-base font-semibold">Departamentos</div>
+              <div className="mx-1 flex flex-row justify-center align-middle">
+                <label htmlFor="all" className="mx-2 mt-[5px]">
+                  Seleccionar todo
+                </label>
+                <input
+                  type="checkbox"
+                  name="all"
+                  checked={Object.keys(filters.hideDepartments).length === 0}
+                  onChange={(e) => handleDepSelectAll(e.target.checked)}
+                />
+              </div>
+            </div>
+            <div className="h-[calc(100%-2rem)] overflow-y-auto px-1">
+              {Object.entries(visibleDepartments).map(([country, departments]) => (
+                <div key={country}>
+                  {country}
+                  {departments.map((department) => (
+                    <div key={department} className="flex items-center border-b border-b-tint-01 p-1 last-of-type:border-0 hover:bg-tint-02">
+                      <input
+                        type="checkbox"
+                        name={department}
+                        id={department}
+                        checked={!filters.hideDepartments?.includes(department)}
+                        onChange={(e) => handleDepCheckboxChange(department, e.target.checked)}
+                        className="mr-2"
+                      />
+                      <label htmlFor={department} className="w-full">
+                        {department}
+                      </label>
+                    </div>
+                  ))}
+                  <hr />
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       )}
     </div>
   )
-}
-
-interface FilterCheckboxesProps {
-  data: DB
-  filters: MarkerFilters
-  setFilters: React.Dispatch<React.SetStateAction<MarkerFilters>>
-  ref: React.RefObject<HTMLDivElement>
-  items: string[]
-  title: string
-  filterKey: 'hideCountries' | 'hideMunicipalities' | 'hideDepartments'
-  childrenKeys: ('hideMunicipalities' | 'hideDepartments')[]
 }
 
 export default CountryControl
