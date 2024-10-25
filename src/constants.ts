362aa98354338b396f6ed6f199c8b32decb4922b
Tsuni <akamitsunami@gmail.com>
2024-10-24 20:55:49 -0500

    Add Pacific Ocean and Carribean Sea "countries".

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,29 @@
+import { PeliasGeoJSONFeature } from '@stadiamaps/api';
 import { LatLngExpression } from 'leaflet'
 export const INITIAL_BOUNDS = [0, -70] as LatLngExpression // Centered on Colombia
 export const INITIAL_ZOOM = 5
+
+export const ADDITIONAL_FEATURES: PeliasGeoJSONFeature[] = [
+  {
+    type: "Feature",
+    properties: {
+      label: "Océano Pacífico",
+      country_a: "PO"
+    },
+    geometry: {
+      type: "Point",
+      coordinates: [3, -99]
+    }
+  },
+  {
+    type: "Feature",
+    properties: {
+      label: "Mar Caribe",
+      country_a: "CS"
+    },
+    geometry: {
+      type: "Point",
+      coordinates: [14, -75]
+    }
+  }
+];
