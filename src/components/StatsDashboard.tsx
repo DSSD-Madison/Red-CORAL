8f0980a11fbdfc2071c764855981e498d3cba8cb
TetraTsunami <78718829+TetraTsunami@users.noreply.github.com>
2024-11-05 12:12:14 -0600

    Revert package-lock and remove unused import

--- src/components/StatsDashboard.tsx
+++ src/components/StatsDashboard.tsx
@@ -1,14 +1,12 @@
 // StatsDashboard.tsx
-import { Incident, DB } from 'types';
-import React from 'react';
-
+import { DB } from 'types'
+import React from 'react'
 
 interface StatsDashboardProps {
   data: DB
-  /*incidentID: keyof DB['Incidents'] | null*/
 }
 
-const StatsDashboard: React.FC<StatsDashboardProps> = ({data}) => {
+const StatsDashboard: React.FC<StatsDashboardProps> = ({ data }) => {
   const incidents = data.Incidents
 
   return (
@@ -16,41 +14,34 @@ const StatsDashboard: React.FC<StatsDashboardProps> = ({data}) => {
       <h1 className="text-2xl font-semibold">Statistics Dashboard</h1>
       <p>This page is currently under development. Statistics will be available here soon.</p>
       <br></br>
-      <div className="m-8 p-4 overflow-hidden border border-black rounded-lg">
-      <table className="table-fixed w-full ">
-                <tr> 
-                    <th className="font-normal pb-2 break-words ">País</th>
-                    <th className="font-normal pb-2 break-words">Tipo de evento</th>
-                    <th className="font-normal pb-2 break-words ">Actividad</th>
-                    <th className="font-normal pb-2 break-words ">Fecha</th>
-                    <th className="font-normal pb-2 break-words ">Descripción</th>
-                    <th className="font-normal pb-2 break-words ">Departamento</th>
-                    <th className="font-normal pb-2 break-words ">Municipio</th>
-                </tr>
-          
-               
-                
-                      {Object.entries(incidents).map(([id, incident]) => (
-                        <tr key={id} className="text-gray-500">
-          
-                          <td className="p-4 text-left break-words border-t border-black">
-                           {incident.country}
-                          </td>
-                          <td className="p-4 text-left break-words border-t border-black">{data.Types[incident.typeID].name}</td>
-                          <td className="p-4 text-left break-words border-t border-black">{data.Categories[data.Types[incident.typeID].categoryID].name}</td>
-                          <td className="p-4 text-left break-words border-t border-black">{incident.dateString}</td>
-                          <td className="p-4 text-left break-words border-t border-black">{incident.description}</td>
-                          <td className="p-4 text-left break-words border-t border-black">{incident.department}</td>
-                          <td className="p-4 text-left break-words border-t border-black">{incident.municipality}</td>
-                        </tr>
-                      ))}
-                    
-            </table>
-            </div>
-    
+      <div className="m-8 overflow-hidden rounded-lg border border-black p-4">
+        <table className="w-full table-fixed">
+          <tr>
+            <th className="break-words pb-2 font-normal">País</th>
+            <th className="break-words pb-2 font-normal">Tipo de evento</th>
+            <th className="break-words pb-2 font-normal">Actividad</th>
+            <th className="break-words pb-2 font-normal">Fecha</th>
+            <th className="break-words pb-2 font-normal">Descripción</th>
+            <th className="break-words pb-2 font-normal">Departamento</th>
+            <th className="break-words pb-2 font-normal">Municipio</th>
+          </tr>
+
+          {Object.entries(incidents).map(([id, incident]) => (
+            <tr key={id} className="text-gray-500">
+              <td className="break-words border-t border-black p-4 text-left">{incident.country}</td>
+              <td className="break-words border-t border-black p-4 text-left">{data.Types[incident.typeID].name}</td>
+              <td className="break-words border-t border-black p-4 text-left">{data.Categories[data.Types[incident.typeID].categoryID].name}</td>
+              <td className="break-words border-t border-black p-4 text-left">{incident.dateString}</td>
+              <td className="break-words border-t border-black p-4 text-left">{incident.description}</td>
+              <td className="break-words border-t border-black p-4 text-left">{incident.department}</td>
+              <td className="break-words border-t border-black p-4 text-left">{incident.municipality}</td>
+            </tr>
+          ))}
+        </table>
+      </div>
     </div>
-  );
-};
+  )
+}
 
 export default StatsDashboard;
 
