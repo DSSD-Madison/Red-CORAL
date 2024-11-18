cd681fc2d34e61fbf2f50727d1df06f2c3db38c6
SlashStars1 <126016850+SlashStars1@users.noreply.github.com>
2024-11-17 20:19:48 -0600

    [feat] Links to map on statistics table (#77)
    * link to zoom ins
    
    * finished linking
    
    * Delete src/components/Map.tsx
    
    * Remove logging & flyto animation
    
    * Replace link text with link icon
    
    ---------
    
    Co-authored-by: Sam N <78718829+TetraTsunami@users.noreply.github.com>

--- src/components/IncidentTable.tsx
+++ src/components/IncidentTable.tsx
@@ -1,6 +1,7 @@
 import { formatDateString } from '@/utils'
 import { DB, Incident } from 'types'
-
+import { Link } from 'react-router-dom';
+import { LucideLink } from 'lucide-react'
 const IncidentTable = ({ data, incidents }: { data: DB; incidents: [string, Incident][] }) => {
   return (
     <div className="my-8 w-full overflow-x-auto rounded-lg border border-black p-4 md:w-full">
@@ -14,6 +15,7 @@ const IncidentTable = ({ data, incidents }: { data: DB; incidents: [string, Inci
             <th className="break-words pb-2 font-normal">Descripción</th>
             <th className="break-words pb-2 font-normal">Departamento</th>
             <th className="break-words pb-2 font-normal">Municipio</th>
+            <th className="break-words pb-2 font-normal">Ubicación</th>
           </tr>
           {incidents.map(([id, incident]) => (
             <tr key={id} className="text-gray-500">
@@ -21,9 +23,14 @@ const IncidentTable = ({ data, incidents }: { data: DB; incidents: [string, Inci
               <td className="break-words border-t border-black p-4 text-left">{data.Types[incident.typeID].name}</td>
               <td className="break-words border-t border-black p-4 text-left">{data.Categories[data.Types[incident.typeID].categoryID].name}</td>
               <td className="break-words border-t border-black p-4 text-left">{formatDateString(incident.dateString)}</td>
-              <td className="lg:max-w-[33vw] max-w-[250px] break-words border-t border-black p-4 text-left">{incident.description}</td>
+              <td className="max-w-[250px] break-words border-t border-black p-4 text-left lg:max-w-[33vw]">{incident.description}</td>
               <td className="break-words border-t border-black p-4 text-left">{incident.department}</td>
               <td className="break-words border-t border-black p-4 text-left">{incident.municipality}</td>
+              <td className="break-words border-t border-black p-4 text-left">
+                <Link to="/" state={{ coord: incident.location }} className="hover:text-blue-400">
+                  <LucideLink />
+                </Link>
+              </td>
             </tr>
           ))}
         </tbody>
