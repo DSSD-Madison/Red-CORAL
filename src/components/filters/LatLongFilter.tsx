b5a8206740b0d87b69bdaf80a891ba8348fd917e
TetraTsunami <78718829+TetraTsunami@users.noreply.github.com>
2024-11-17 20:57:23 -0600

    Translate to Spanish

--- src/components/filters/LatLongFilter.tsx
+++ src/components/filters/LatLongFilter.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { filterProps } from '@/pages/StatsDashboard'
 import BaseFilter from './BaseFilter'
-import { LucideMapPin } from 'lucide-react'
+import { LucideMapPin, LucideTrash2 } from 'lucide-react'
 
 export const calculateDistance = (lat1: number, lon1: number, lat2: number, lon2: number): number => {
   const toRadians = (deg: number) => deg * (Math.PI / 180)
@@ -42,28 +42,28 @@ const LatLongFilter: React.FC<filterProps> = ({ id, dispatch }) => {
   }
 
   return (
-    <BaseFilter icon={<LucideMapPin />} text={`Location: (${latitude}, ${longitude}) Radius: ${radius}`}>
-      <button onClick={removeThisFilter} className="text-red-600">
-        Remove Filter
+    <BaseFilter icon={<LucideMapPin />} text={`Ubicación: (${latitude || 0}, ${longitude || 0}) Radio: ${radius || 'N/A'}`}>
+      <button onClick={removeThisFilter} className="absolute right-2 top-1 h-4 w-4 text-red-600" title="Eliminar Filtro">
+        <LucideTrash2 size={20} />
       </button>
       <br />
       <input
         type="number"
-        placeholder="Latitude"
+        placeholder="Latitud"
         value={latitude}
         onChange={(e) => setLatitude(e.target.value)}
         className="rounded-md border border-gray-300 p-1"
       />
       <input
         type="number"
-        placeholder="Longitude"
+        placeholder="Longitud"
         value={longitude}
         onChange={(e) => setLongitude(e.target.value)}
         className="rounded-md border border-gray-300 p-1"
       />
       <input
         type="number"
-        placeholder="Radius"
+        placeholder="Radio"
         value={radius}
         onChange={(e) => setRadius(e.target.value)}
         className="rounded-md border border-gray-300 p-1"
