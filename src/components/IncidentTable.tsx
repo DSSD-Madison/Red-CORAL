5754e7f121a0a71b08d09e897161df34fd3fdff4
Tsuni <78718829+TetraTsunami@users.noreply.github.com>
2024-11-22 16:33:11 -0600

    [feat] Infinite scroll for IncidentTable

--- src/components/IncidentTable.tsx
+++ src/components/IncidentTable.tsx
@@ -2,9 +2,26 @@ import { formatDateString } from '@/utils'
 import { DB, Incident } from 'types'
 import { Link } from 'react-router-dom';
 import { LucideLink } from 'lucide-react'
+import { useState, useEffect, useRef } from 'react'
+
+const INCIDENT_INCREMENT = 10
+
 const IncidentTable = ({ data, incidents }: { data: DB; incidents: [string, Incident][] }) => {
+  const [visibleCount, setVisibleCount] = useState(10)
+  const tableContainerRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (tableContainerRef.current && window.innerHeight + window.scrollY >= tableContainerRef.current.offsetHeight) {
+        setVisibleCount((prev) => prev + INCIDENT_INCREMENT)
+      }
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
   return (
-    <div className="my-4 w-full overflow-x-auto rounded-lg border border-black p-4 md:w-full">
+    <div ref={tableContainerRef} className="my-4 w-full overflow-x-auto rounded-lg border border-black p-4 md:w-full">
       <table className="w-full min-w-[800px] table-auto md:min-w-0 md:max-w-full">
         <tbody>
           <tr>
@@ -17,7 +34,7 @@ const IncidentTable = ({ data, incidents }: { data: DB; incidents: [string, Inci
             <th className="break-words pb-2 font-normal">Municipio</th>
             <th className="break-words pb-2 font-normal">Ubicación</th>
           </tr>
-          {incidents.map(([id, incident]) => (
+          {incidents.slice(0, visibleCount).map(([id, incident]) => (
             <tr key={id} className="text-gray-500">
               <td className="break-words border-t border-black p-4 text-left">{incident.country}</td>
               <td className="break-words border-t border-black p-4 text-left">{data.Types[incident.typeID].name}</td>
