c7e1acc45ffc55e4b01de9ea52c32cc560980a53
MysteryCoder456 <rehatbir.singh@gmail.com>
2024-11-25 16:58:47 -0600

    feat: added a simpler map in statics dashboard

--- src/pages/StatsDashboard.tsx
+++ src/pages/StatsDashboard.tsx
@@ -5,6 +5,7 @@ import IncidentTable from '@/components/IncidentTable'
 import StatisticsFilterBar from '@/components/StatisticsFilterBar'
 import { calculateBounds } from '@/utils'
 import DummyGraph from '@/components/graphs/DummyGraph'
+import StatisticsFilterMap from '@/components/StatisticsFilterMap'
 
 export type filterDispatchType = { type: 'ADD_FILTER' | 'REMOVE_FILTER' | 'UPDATE_FILTER'; payload: Partial<filterType> }
 
@@ -70,7 +71,7 @@ const StatsDashboard: React.FC<StatsDashboardProps> = ({ data }) => {
   const filteredBounds = calculateBounds(Object.fromEntries(filteredIncidents))
 
   return (
-    <div className="p-4">
+    <div className="h-full p-4">
       <div className="flow-row flex items-center justify-between">
         <h1 className="text-2xl font-semibold">Estadísticas</h1>
         <button className="m-1 rounded-md px-2 py-1 hover:bg-black hover:bg-opacity-10" onClick={() => setIsShowingMap(!isShowingMap)}>
@@ -79,7 +80,7 @@ const StatsDashboard: React.FC<StatsDashboardProps> = ({ data }) => {
       </div>
       <StatisticsFilterBar data={data} filters={filters.filters} dispatchFilters={dispatchFilters} />
       {isShowingMap ? (
-        <p>Map goes here</p>
+        <StatisticsFilterMap data={data} />
       ) : (
         <>
           <div className="my-4 flex flex-row flex-wrap gap-4">
