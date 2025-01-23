4efd39477402f2156e1a96ad171fde94c2e9ef55
Tsuni <78718829+TetraTsunami@users.noreply.github.com>
2025-01-22 22:07:27 -0600

    [feat] Sort stats dashboard incidents

--- src/pages/StatsDashboard.tsx
+++ src/pages/StatsDashboard.tsx
@@ -66,11 +66,19 @@ const filterReducer = (state: filterState, action: filterDispatchType) => {
 
 const StatsDashboard: React.FC<StatsDashboardProps> = ({ data }) => {
   const [isShowingMap, setIsShowingMap] = useState(false)
-  const incidents: [string, Incident][] = Object.entries(data.Incidents)
   const [filters, dispatchFilters] = useReducer(filterReducer, { index: 0, filters: [] })
-  const filteredIncidents = useMemo(() => {
-    return incidents.filter(([, incident]) => filters.filters.every((filter) => (filter.operation ? filter.operation(incident) : true)))
-  }, [incidents, filters])
+  const incidents: [string, Incident][] = Object.entries(data.Incidents)
+  const sortedIncidents = useMemo(
+    () =>
+      incidents.sort(
+        ([, a], [, b]) => a.dateString.localeCompare(b.dateString) || a.location.lat - b.location.lat || a.location.lng - b.location.lng
+      ),
+    [incidents]
+  )
+  const filteredIncidents = useMemo(
+    () => sortedIncidents.filter(([, incident]) => filters.filters.every((filter) => (filter.operation ? filter.operation(incident) : true))),
+    [sortedIncidents, filters]
+  )
   const filteredBounds = calculateBounds(Object.fromEntries(filteredIncidents))
 
   return (
