21325bc0b72f52e90be354f27e5913f8e6e3e1f6
Tsuni <78718829+TetraTsunami@users.noreply.github.com>
2025-04-05 21:15:13 -0500

    fix: table styles

--- src/components/IncidentTable.tsx
+++ src/components/IncidentTable.tsx
@@ -23,28 +23,28 @@ const IncidentTable = ({ incidents }: { incidents: [string, Incident][] }) => {
   }, [])
 
   return (
-    <table ref={tableRef} className="w-full min-w-[800px] table-auto md:min-w-0 md:max-w-full">
+    <table ref={tableRef} className="w-full min-w-[600px] table-auto md:min-w-0 md:max-w-full">
       <tbody>
         <tr>
           <th className="pb-2">País</th>
           <th className="w-[5%] pb-2">Tipos de incidentes</th>
-          <th className="pb-">Actividad</th>
+          <th className="pb-2">Actividad</th>
           <th className="pb-2">Fecha</th>
-          <th className="pb-2">Descripción</th>
+          <th className="min-w-[250px] pb-2">Descripción</th>
           <th className="pb-2">Departamento</th>
           <th className="pb-2">Municipio</th>
           <th className="pb-2">Ubicación</th>
         </tr>
         {incidents.slice(0, visibleCount).map(([id, incident]) => (
           <tr key={id} className="text-gray-500">
-            <td className="break-words border-t border-black p-4 text-left">{incident.country}</td>
-            <td className="break-words border-t border-black p-4 text-left text-sm">{typeIDtoTypeName(db, incident.typeID)}</td>
-            <td className="break-words border-t border-black p-4 text-left">{typeIDtoCategory(db, incident.typeID).name}</td>
-            <td className="break-words border-t border-black p-4 text-left">{formatDateString(incident.dateString)}</td>
-            <td className="max-w-[250px] break-words border-t border-black p-4 text-left text-sm lg:max-w-[33vw]">{incident.description}</td>
-            <td className="break-words border-t border-black p-4 text-left">{incident.department}</td>
-            <td className="break-words border-t border-black p-4 text-left">{incident.municipality}</td>
-            <td className="break-words border-t border-black p-4 text-left">
+            <td className="break-words border-t border-black p-2 text-left">{incident.country}</td>
+            <td className="break-words border-t border-black p-2 text-left text-sm">{typeIDtoTypeName(db, incident.typeID)}</td>
+            <td className="break-words border-t border-black p-2 text-left">{typeIDtoCategory(db, incident.typeID).name}</td>
+            <td className="break-words border-t border-black p-2 text-left">{formatDateString(incident.dateString)}</td>
+            <td className="break-words border-t border-black p-2 text-left text-sm lg:max-w-[33vw]">{incident.description}</td>
+            <td className="break-words border-t border-black p-2 text-left">{incident.department}</td>
+            <td className="break-words border-t border-black p-2 text-left">{incident.municipality}</td>
+            <td className="break-words border-t border-black p-2 text-left">
               <Link to="/" state={{ coord: incident.location }} className="hover:text-blue-400">
                 <LucideLink />
               </Link>
